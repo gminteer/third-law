1879c9f51535581ea819c646c23a3a1e5f166584
gminteer <gminteer@gmail.com>
2020-10-01 11:00:40 -0400

    minor bugfixes, error handler gets its own file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,56 +1,9 @@
 const router = require('express').Router();
 
-// Can't find an API in mongodb/mongoose to look this up
-const MONGO_ERR_DUPLICATE_KEY = 11000;
-
 module.exports = (services) => {
   router.use('/users', require('./user')(services));
   router.use('/thoughts', require('./thought')(services));
   // Common API route error handler
-  router.use((err, req, res, next) => {
-    console.error(err);
-    switch (err.name) {
-      case 'NotFoundError': {
-        return res.status(404).json({
-          message: err.message,
-          details: {name: 'NOT_FOUND', [err.path]: err.query},
-        });
-      }
-      case 'DuplicateError': {
-        return res.status(400).json({
-          message: err.message,
-          details: {name: 'DUPLICATE', [err.path]: err.value},
-        });
-      }
-      case 'CastError': {
-        return res.status(400).json({
-          message: `Invalid object ID: ${err.value}`,
-          details: {name: 'BAD_ID', id: err.value},
-        });
-      }
-      case 'ValidationError': {
-        const paths = Object.keys(err.errors);
-        return res.status(400).json({
-          message: `Vailed validation on path(s): ${paths}`,
-          details: {name: 'FAILED_VALIDATION', paths},
-        });
-      }
-      case 'MongoError': {
-        switch (err.code) {
-          case MONGO_ERR_DUPLICATE_KEY: {
-            // Can't find a better way to get error details than just regex parsing err.message
-            const [, index] = err.message.match(/index: (\w+)/);
-            return res.status(422).json({
-              message: `Duplicate key: '${index}'`,
-              details: {name: 'DUPLICATE_KEY', index},
-            });
-          }
-        }
-      }
-    }
-    // If you're reading this you should write a better handler for this error...
-    if (process.env.NODE_ENV !== 'production') debugger;
-    return res.status(500).json({message: err.message, err});
-  });
+  router.use(require('./error-handler'));
   return router;
 };
--- services/thought.js
+++ services/thought.js
@@ -16,7 +16,7 @@ module.exports = ({User, Thought}, {NotFoundError}) => ({
   async createThought(data) {
     if (!data) return;
     const user = await User.findOne({username: data.username});
-    if (!user) throw new NotFoundError('users', data.username);
+    if (!user) throw new NotFoundError('users', 'username', data.username);
     const sanitizedData = Object.fromEntries(
       Object.entries(data).filter(([key]) => WRITABLE.includes(key))
     );
@@ -29,7 +29,7 @@ module.exports = ({User, Thought}, {NotFoundError}) => ({
   async updateThought(_id, data) {
     if (!data) return;
     const user = await User.findOne({username: data.username});
-    if (!user) throw new NotFoundError('users', data.username);
+    if (!user) throw new NotFoundError('users', 'username', data.username);
     const sanitizedData = Object.fromEntries(
       Object.entries(data).filter(([key]) => WRITABLE.includes(key))
     );
@@ -46,12 +46,22 @@ module.exports = ({User, Thought}, {NotFoundError}) => ({
     return thought;
   },
 
+  async getReaction(thoughtId, reactionId) {
+    const thought = await Thought.findById(thoughtId);
+    if (!thought) throw new NotFoundError('thoughts', '_id', thoughtId);
+    const reaction = thought.reactions.find(
+      (reaction) => reaction.reactionId.toHexString() === reactionId
+    );
+    if (!reaction) throw new NotFoundError('reactions', 'reactionId', reactionId);
+    return reaction;
+  },
+
   async createReaction(thoughtId, data) {
     if (!data) return;
     const user = await User.findOne({username: data.username});
-    if (!user) throw new NotFoundError('users', data.username);
+    if (!user) throw new NotFoundError('users', 'username', data.username);
     const thought = await Thought.findById(thoughtId);
-    if (!thought) throw new NotFoundError('thoughts', thoughtId);
+    if (!thought) throw new NotFoundError('thoughts', '_id', thoughtId);
     const sanitizedData = Object.fromEntries(
       Object.entries(data).filter(([key]) => WRITABLE.includes(key))
     );
@@ -60,42 +70,31 @@ module.exports = ({User, Thought}, {NotFoundError}) => ({
     return thought;
   },
 
-  async getReaction(thoughtId, reactionId) {
-    const thought = await Thought.findById(thoughtId);
-    if (!thought) throw new NotFoundError('thoughts', thoughtId);
-    const reaction = thought.reactions.find(
-      (reaction) => reaction.reactionId.toHexString() === reactionId
-    );
-    if (!reaction) throw new NotFoundError('reactions', reactionId);
-    return reaction;
-  },
-
   async updateReaction(thoughtId, reactionId, data) {
     if (!data) return;
     const user = await User.findOne({username: data.username});
-    if (!user) throw new NotFoundError('users', data.username);
+    if (!user) throw new NotFoundError('users', 'username', data.username);
     const thought = await Thought.findById(thoughtId);
-    if (!thought) throw new NotFoundError('thoughts', thoughtId);
+    if (!thought) throw new NotFoundError('thoughts', '_id', thoughtId);
     const reaction = thought.reactions.find(
       (reaction) => reaction.reactionId.toHexString() === reactionId
     );
-    if (!reaction) throw new NotFoundError('reactions', reactionId);
+    if (!reaction) throw new NotFoundError('reactions', 'reactionId', reactionId);
     Object.entries(data)
       .filter(([key]) => WRITABLE.includes(key))
       .forEach(([key, value]) => (reaction[key] = value));
     await thought.save();
-    return thought;
+    return reaction;
   },
 
   async deleteReaction(thoughtId, reactionId) {
     const thought = await Thought.findById(thoughtId);
-    if (!thought) throw new NotFoundError('thoughts', thoughtId);
+    if (!thought) throw new NotFoundError('thoughts', '_id', thoughtId);
     const reaction = thought.reactions.find(
       (reaction) => reaction.reactionId.toHexString() === reactionId
     );
-    if (!reaction) throw new NotFoundError('reactions', reactionId);
-    thought.reactions.pull(reaction);
-    await thought.save();
-    return thought;
+    if (!reaction) throw new NotFoundError('reactions', 'reactionId', reactionId);
+    await Thought.findByIdAndUpdate(thoughtId, {$pull: {reactions: {reactionId}}}, {new: true});
+    return reaction;
   },
 });
